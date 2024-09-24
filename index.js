963cd8cb5154cde50f581d9ad86193af9e7b7163
Khaled Abbara <148062226+Khaled-Abbara@users.noreply.github.com>
2024-09-25 06:53:46 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+screen = [];
+
+const pointer = {
+  symbol: "|",
+  index: 0,
+};
+
+let screenContent = document.getElementById("screen");
+
+function inputNumber(number) {
+  screen.splice(pointer.index, 1);
+  screen.splice(pointer.index, 0, number);
+  pointer.index++;
+
+  console.log("i = " + pointer.index);
+  return displayScreen();
+}
+
+function clearing() {
+  screen.splice(pointer.index, 1);
+  pointer.index = 0;
+  screen = [];
+
+  console.log("i = " + pointer.index);
+  return displayScreen();
+}
+
+function deleting() {
+  screen.splice(pointer.index, 1);
+  if (pointer.index > 0) {
+    pointer.index--;
+  } else if (pointer.index == 0) {
+    return;
+  } else {
+    screen.splice(pointer.index, 1);
+    console.log("i = " + pointer.index);
+  }
+  return displayScreen();
+}
+
+function moveCursorBackward() {
+  if (pointer.index == 0) {
+    screen.splice(pointer.index, 1);
+    pointer.index = screen.length;
+    console.log("i = " + pointer.index);
+  } else {
+    screen.splice(pointer.index, 1);
+    pointer.index--;
+    console.log("i = " + pointer.index);
+  }
+  return displayScreen();
+}
+
+function moveCursorForward() {
+  if (pointer.index == screen.length - 1) {
+    screen.splice(pointer.index, 1);
+    pointer.index = 0;
+    console.log("i = " + pointer.index);
+  } else {
+    screen.splice(pointer.index, 1);
+    pointer.index++;
+    console.log("i = " + pointer.index);
+  }
+  return displayScreen();
+}
+
+function displayScreen() {
+  screen.splice(pointer.index, 0, pointer.symbol);
+  let joinedScreen = screen.join("");
+  screenContent.innerHTML = joinedScreen;
+}
